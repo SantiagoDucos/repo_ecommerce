db6778c7b8de3eb7c4a99478617c6392fc763ad9
Santiago Ducós <santidu200@gmail.com>
2020-08-22 03:19:05 -0300

    Entrega 2 (1)

--- js/login.js
+++ js/login.js
@@ -1,26 +1,24 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
 // CUANDO TERMINE DE CARGARSE EL DOM
 document.addEventListener("DOMContentLoaded", function(e){
 
-  // SE CREAR UN EVENTO, QUE ESCUCHA AL BOTÓN DE INGRESAR 
-  document.getElementById("buttonIngresoLogin").addEventListener("click", function (e) {
+    // SE CREAR UN EVENTO, QUE ESCUCHA AL BOTÓN DE INGRESAR 
+    document.getElementById("buttonIngresoLogin").addEventListener("click", function (e) {
 
     // EXTRAEMOS LOS DATOS
-    let email = document.getElementById("inputEmailLogin");
-    let password = document.getElementById("inputPasswordLogin");
-    // CREAMOS UNA VARIBLE QUE VERIFICA QUE LOS CAMPOS ENTÉN COMPLETOS
+    let inputEmail = document.getElementById("inputEmailLogin");
+    let inputPassword = document.getElementById("inputPasswordLogin");
+        
+    // VERIFICAMO QUE LOS CAMPOS ESTÉN COMPLETOS
     let camposCompletos = true;
-
-    if (password.value === '' || email.value === ''){
-      camposCompletos = false;
-      alert("Debes ingresar tus datos para continuar.");
+    if (inputEmail.value === '' || inputPassword.value === ''){
+        camposCompletos = false;
+        alert("Debes ingresar tus datos para continuar.");
     }
 
-    if (camposCompletos){
-      window.location = 'home.html';
+        if (camposCompletos) { // SI LOS DATOS ESTÁN COMPLETOS
+        // GUARDAMOS EN EL STORAGE LA INFORMACION INGRESADA
+        localStorage.setItem('user-logged', JSON.stringify({ email: inputEmail.value }));
+        window.location = 'home.html';
     }
 
   });
